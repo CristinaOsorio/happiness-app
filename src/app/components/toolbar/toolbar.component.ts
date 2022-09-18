0e9f24db1b1570b455a2a5033b9d9df0805f7ce8
CristinaOsorio <macry_08@hotmail.com>
2022-09-17 23:06:28 -0500

    Integracion de una ventana de dialogo en ToolbarComponent

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,19 +1,28 @@
+import { PeopleTableComponent } from './../people-table/people-table.component';
 import { Component, OnInit } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @Component({
   standalone: true,
-  imports: [MatToolbarModule, MatIconModule],
+  imports: [MatToolbarModule, MatIconModule, MatDialogModule],
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.scss']
 })
 export class ToolbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit(): void {
   }
 
+  openDialog(enterAnimationDuration: string, exitAnimationDuration: string): void {
+    this.dialog.open(PeopleTableComponent, {
+      enterAnimationDuration,
+      exitAnimationDuration,
+    });
+  }
 }
