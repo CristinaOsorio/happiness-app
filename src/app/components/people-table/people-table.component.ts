d82358d7b43a1f877e2c15598b0d7ef8445f5963
CristinaOsorio <macry_08@hotmail.com>
2022-09-17 22:50:07 -0500

    Ordenar datos en PeopleTableComponent

--- src/app/components/people-table/people-table.component.ts
+++ src/app/components/people-table/people-table.component.ts
@@ -3,11 +3,12 @@ import { Person } from './../../models/peope.model';
 import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
-import { MatSort } from '@angular/material/sort';
+import { MatSort, MatSortModule, Sort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatTableModule } from '@angular/material/table';
 import { MatInputModule } from '@angular/material/input';
 import { People } from 'src/app/data';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
 
 @Component({
   standalone: true,
@@ -17,6 +18,7 @@ import { People } from 'src/app/data';
     MatTableModule,
     MatPaginatorModule,
     MatFormFieldModule,
+    MatSortModule
   ],
   selector: 'app-people-table',
   styleUrls: ['people-table.component.scss'],
@@ -29,7 +31,7 @@ export class PeopleTableComponent implements AfterViewInit {
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
 
-  constructor() {
+  constructor(private _liveAnnouncer: LiveAnnouncer) {
     this.dataSource = new MatTableDataSource(People);
   }
 
@@ -44,6 +46,21 @@ export class PeopleTableComponent implements AfterViewInit {
 
     if (this.dataSource.paginator) {
       this.dataSource.paginator.firstPage();
+    }
+  }
+
+  announceSortChange(value: any) {
+    console.log(value);
+
+    let sortState: Sort = value as Sort;
+    // This example uses English messages. If your application supports
+    // multiple language, you would internationalize these strings.
+    // Furthermore, you can customize the message to add additional
+    // details about the values being sorted.
+    if (sortState.direction) {
+      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+    } else {
+      this._liveAnnouncer.announce('Sorting cleared');
     }
   }
 }
